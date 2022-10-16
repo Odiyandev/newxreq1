1eb3eb93535313cad5b5d0af4a700a726cf44c39
ODIYAN_DEV <100528793+Odiyandev@users.noreply.github.com>
2022-10-16 13:34:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -29,7 +29,7 @@ This methord works on any chat, Just type @NewxReq_bot and then leave a space an
 ○Dont Spam Me...🤒
 """
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/NewxReq_bot>Jack Sparrow</a>
+✯ 𝚘𝚠𝚗𝚎𝚛: <a href=https://t.me/Tutru_tutru>TUTRU</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
